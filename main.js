0fe5a2011cd4f998c351a56217f719cc500694c3
jkcoxson@gmail.com <jkcoxson@gmail.com>
2021-07-14 16:06:47 -0600

    Make the example more clear

--- main.js
+++ main.js
@@ -1,51 +1,51 @@
 const { EventEmitter } = require('events');
-const { resolve } = require('path');
-const fs = require('fs');
-const commandRunner = require('../../commandRunner')
-const mcCommandRunner = require('../minecraft/mcCommandRunner')
-
-let logger
-let camellib
 
 module.exports = class plugin extends EventEmitter {
-    constructor(parameters){
-        super();
-        logger = parameters.logger
-        camellib = parameters.camellib
-        parameters.logger.info("Example plugin has been loaded")
-    }   
     /**
      * 
-     * @param {commandRunner|mcCommandRunner} lol 
+     * @param {import('../../mappedClass')} parameters 
      */
-    singCommand(lol){
-        if(lol.source=='discord'){
-            lol.interaction.reply("Singing now")
-            for(let i = 0; i<lol.interaction.options.get('value').value; i++){
-                lol.interaction.channel.send("la")
+    constructor(parameters) {
+        super();
+        parameters.logger.info('Example plugin has been loaded');
+    }
+    /**
+     * 
+     * @param {import('../../commandRunner')|import('../minecraft/mcCommandRunner')} lol 
+     */
+    singCommand(lol) {
+        if (lol.source == 'discord') {
+            lol.interaction.reply('Singing now');
+            for (let i = 0; i < lol.interaction.options.get('value').value; i++) {
+                lol.interaction.channel.send('la');
             }
         }
-        if(lol.source=='minecraft'){
+        if (lol.source == 'minecraft') {
             lol.game.socket.write(JSON.stringify({
-                "packet":"command",
-                "command":"tellraw @a {\"text\":\"Singing now\"}"
-            })+"\n");
-            for (let i = 0; i < parseInt(lol.args[0]); i++){
+                'packet': 'command',
+                'command': 'tellraw @a {"text":"Singing now"}'
+            }) + '\n');
+            for (let i = 0; i < parseInt(lol.args[0]); i++) {
                 lol.game.socket.write(JSON.stringify({
-                    "packet":"command",
-                    "command":"tellraw @a {\"text\":\"la\"}"
-                })+"\n");
+                    'packet': 'command',
+                    'command': 'tellraw @a {"text":"la"}'
+                }) + '\n');
             }
         }
     }
-    onChat(message){
-        if(message.content.includes("example")){
-            return(true)
+    /**
+     * 
+     * @param {import('../minecraft/chatMessage')} message 
+     * @returns {undefined}
+     */
+    onChat(message) {
+        if (message.content.includes('example')) {
+            return (true);
         }
-        if(message.content.includes("yeet")){
-            message.content = message.content.replace("yeet","lol")
-            return(false)
+        if (message.content.includes('yeet')) {
+            message.content = message.content.replace('yeet', 'lol');
+            return (false);
         }
     }
 
-}
+};
